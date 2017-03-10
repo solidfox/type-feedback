8b7ec933276548adb87e60d6bb1866716d226bb3
Daniel Schlaug <daniel.schlaug@gmail.com>
2017-03-10 13:31:41 +0100

    Initial Commit

---
+++ TypeFeedback/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TypeFeedback
+//
+//  Created by Daniel Schlaug on 2017-03-10.
+//  Copyright © 2017 Daniel Schlaug. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ TypeFeedback/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TypeFeedback
+//
+//  Created by Daniel Schlaug on 2017-03-10.
+//  Copyright © 2017 Daniel Schlaug. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
